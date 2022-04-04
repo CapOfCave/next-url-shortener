54409a2b1f35ba6de10993fb970d840ee5b4d316
Lars Kecker <lars.kecker2009@gmail.com>
2022-04-04 20:40:20 +0200

    use icon for color mode toggle

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,18 @@
-import { Button, Flex, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Button, Flex, Tooltip, useColorMode } from "@chakra-ui/react";
 
 const Navbar: React.FC = () => {
 
     const { colorMode, toggleColorMode } = useColorMode()
 
+    const tooltip = colorMode === 'light' ? "Switch to Dark Design" : "Switch to Light Design";
     return (
         <Flex justify='flex-end' padding='2'>
-            <Button onClick={toggleColorMode}>
-                Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-            </Button>
+            <Tooltip label={tooltip}>
+                <Button onClick={toggleColorMode}>
+                    {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                </Button>
+            </Tooltip>
         </Flex>
     )
 }
